e029665c71971799bd52d51ac6c11783025ae731
Lachlan Brown <lachlan.brown8@live.com.au>
2015-12-16 01:33:10 +1100

    Bunch o' thing happening. Header and footer now able to implemented across all pages.
    https://youtu.be/SuL2tSlJtSk?t=323
    
    https://youtu.be/IgMH0zrzAqc?t=1375

---
+++ DLI/js/index.js
@@ -0,0 +1,12 @@
+window.onscroll = function () {
+  var el = document.getElementsByClassName('header')[0];
+  var className = 'small';
+  if (el.classList) {
+    if (window.scrollY > 10)
+      el.classList.add(className);
+    else
+      el.classList.remove(className);
+  }
+};
+
+/* Got this script from "Campus Online/MDU119 Web Design/Resources/You can - sort of -copy this.zip" */
