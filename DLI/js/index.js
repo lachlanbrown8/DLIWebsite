e4aaa9370ea5e3a87d2d0ce3a29ade5ccc4f2438
unknown <lbrown53@SYD-1540-W.sae.au>
2015-12-16 12:02:59 +1100

    no message

--- DLI/js/index.js
+++ DLI/js/index.js
@@ -1,5 +1,5 @@
 window.onscroll = function () {
-  var el = document.getElementsByClassName('header')[0];
+  var el = document.getElementById('header')[0];
   var className = 'small';
   if (el.classList) {
     if (window.scrollY > 10)
